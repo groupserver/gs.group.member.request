815d56b07662ad9401591862a888dd09cbb664e6
Michael JasonSmith <mpj17@onlinegroups.net>
2011-05-03 16:12:04 +1000

    Removing debugging

--- gs/group/member/request/mesg.py
+++ gs/group/member/request/mesg.py
@@ -8,15 +8,10 @@ from gs.group.base.page import GroupPage
 class RequestMessage(GroupPage):
     def __init__(self, group, request):
         GroupPage.__init__(self, group, request)
-        print 'Here 1'
         self.userId = request.form['userId']
-        print 'Here 2'
         self.adminId = request.form['adminId']
-        print 'Here 3'
         self.email = request.form['email']
-        print 'Here 4'
         self.mesg  = request.form['mesg']
-        print 'Here 5'
 
     @Lazy    
     def userInfo(self):
