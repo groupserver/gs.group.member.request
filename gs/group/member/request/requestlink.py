76d7390a60dfec0905540cbf73684978f4a1604b
Michael JasonSmith <mpj17@onlinegroups.net>
2011-05-23 10:53:24 +1000

    Adding the Request Membership links to the group homepage.

---
+++ gs/group/member/request/requestlink.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from gs.group.member.base.viewlet import MemberViewlet
+
+class RequestLinkViewlet(MemberViewlet):
+    def __init__(self, group, request, view, manager):
+        MemberViewlet.__init__(self, group, request, view, manager)
+
+    @Lazy
+    def show(self):
+        # Show the Request links if the user is not a member, and the 
+        #   group is private. Non-members cannot see secret groups, so
+        #   they are not a problem.
+        retval = not(self.isMember) and not(self.viewTopics)
+        return retval
+
