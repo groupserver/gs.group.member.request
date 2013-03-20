ea7c3b99462290fe7ecdd12c088f192fb62b4d39
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-20 16:24:31 +1100

    Code cleanup, with the help of Ninja-IDE.

--- gs/group/member/request/adminlist.py
+++ gs/group/member/request/adminlist.py
@@ -1,15 +1,14 @@
-# coding=utf-8
-from zope.component import createObject
+# -*- coding: utf-8 -*-
 from zope.cachedescriptors.property import Lazy
-from gs.group.member.base.viewlet import GroupAdminViewlet
+from gs.group.member.viewlet import GroupAdminViewlet
 from queries import RequestQuery
 
+
 class RequestMembershipListViewlet(GroupAdminViewlet):
-    
+
     @Lazy
     def requestCount(self):
         rq = RequestQuery()
-        retval = rq.count_current_requests(self.groupInfo.id, 
+        retval = rq.count_current_requests(self.groupInfo.id,
                                             self.siteInfo.id)
         return retval
-
--- gs/group/member/request/requestlink.py
+++ gs/group/member/request/requestlink.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
-from gs.group.member.base.viewlet import MemberViewlet
+from gs.group.member.viewlet import MemberViewlet
+
 
 class RequestLinkViewlet(MemberViewlet):
     def __init__(self, group, request, view, manager):
@@ -8,9 +9,8 @@ class RequestLinkViewlet(MemberViewlet):
 
     @Lazy
     def show(self):
-        # Show the Request links if the user is not a member, and the 
+        # Show the Request links if the user is not a member, and the
         #   group is private. Non-members cannot see secret groups, so
         #   they are not a problem.
         retval = not(self.isMember) and not(self.viewTopics)
         return retval
-
