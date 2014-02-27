a93245b51f1f8346719540284da27d655c4e52fe
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-27 10:26:44 -0800

    Dealing with headers being thrown in the Request form.

--- gs/group/member/request/request.py
+++ gs/group/member/request/request.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,21 +12,18 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-from __future__ import absolute_import
-from datetime import datetime
-import md5
-from pytz import UTC
+from __future__ import absolute_import, unicode_literals
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject, getMultiAdapter
 from zope.formlib import form
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('groupserver')
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from gs.core import to_id, to_ascii
 from gs.group.base import GroupForm
 from gs.group.member.base import user_member_of_group
 from gs.profile.notify.sender import MessageSender
 from gs.profile.email.base.emailuser import EmailUser
-from Products.XWFCore.XWFUtils import convert_int2b62
 from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
 from .interfaces import IGSRequestMembership
 from .queries import RequestQuery
@@ -35,12 +32,14 @@ from .audit import RequestAuditor
 
 class RequestForm(GroupForm):
     form_fields = form.Fields(IGSRequestMembership)
-    label = _(u'Request Membership')
+    label = _('Request membership')
     pageTemplateFileName = 'browser/templates/request.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
 
     def __init__(self, group, request):
-        GroupForm.__init__(self, group, request)
+        super(RequestForm, self).__init__(group, request)
+        h = request.response.getHeader('Content-Type')
+        self.oldContentType = to_ascii(h if h else 'text/html')
 
     @Lazy
     def userInfo(self):
@@ -58,10 +57,9 @@ class RequestForm(GroupForm):
         return retval
 
     def setUpWidgets(self, ignore_request=False):
-        message = _(u'Hi there!\n\nI would like to join ') +\
+        message = _('Hi there!\n\nI would like to join ') +\
             self.groupInfo.name +\
-            _(u'. I think I should be allowed to become a member '
-                u'because...')
+            _('. I think I should be allowed to become a member because...')
 
         if self.userInfo.anonymous:
             fromAddr = ''
@@ -81,7 +79,7 @@ class RequestForm(GroupForm):
 
     @form.action(label=_('Request'), failure='handle_failure')
     def handle_request(self, action, data):
-        self.status = u''
+        self.status = ''
         requestId = self.create_request_id(data['fromAddress'], data['message'])
         self.requestQuery.add_request(requestId, self.userInfo.id,
             data['message'], self.siteInfo.id, self.groupInfo.id)
@@ -96,37 +94,38 @@ class RequestForm(GroupForm):
 
         l = '<a href="%s">%s</a>. ' % (self.groupInfo.relativeURL,
                                         self.groupInfo.name)
-        self.status = _(u'<p>You have requested membership of ') + l +\
-            _(u'You will be contacted by the group administator when '
-                u'your request is considered.</p>')
+        self.status = _('<p>You have requested membership of ') + l +\
+            _('You will be contacted by the group administator when '
+                'your request is considered.</p>')
 
     def send_message(self, fromAddress, adminInfo, message):
         sender = MessageSender(self.context, adminInfo)
-        subject = _(u'Request to Join ') + self.groupInfo.name
+        subject = _('Request to join ') + self.groupInfo.name
         newRequest = self.request
         newRequest.form['userId'] = self.userInfo.id
         newRequest.form['email'] = fromAddress
         newRequest.form['mesg'] = message
         newRequest.form['adminId'] = adminInfo.id
+
         txt = getMultiAdapter((self.context, newRequest),
                             name="request_message.txt")()
         html = getMultiAdapter((self.context, newRequest),
                             name="request_message.html")()
         sender.send_message(subject, txt, html, fromAddress)
 
+        self.request.response.setHeader(to_ascii('Content-Type'),
+                                            self.oldContentType)
+
     def create_request_id(self, fromAddress, message):
         istr = fromAddress + message + self.userInfo.id + \
-            str(datetime.now(UTC)) + self.userInfo.name + \
-            self.groupInfo.id + self.groupInfo.name + \
+            self.userInfo.name + self.groupInfo.id + self.groupInfo.name + \
             self.siteInfo.id + self.siteInfo.name
-        inum = long(md5.new(istr).hexdigest(), 16)
-        retval = str(convert_int2b62(inum))
+        retval = to_id(istr)
         assert retval
-        assert type(retval) == str
         return retval
 
     def handle_failure(self, action, data, errors):
         if len(errors) == 1:
-            self.status = _(u'There was an error:')
+            self.status = _('There was an error:')
         else:
-            self.status = _(u'<p>There were errors:</p>')
+            self.status = _('<p>There were errors:</p>')
